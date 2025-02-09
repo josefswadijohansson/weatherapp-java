9778322b0b73036beee678134a789b17452eb60a
josefswadijohansson <swadijosef@gmail.com>
2025-02-09 15:44:26 +0100

    initial setup

---
+++ src/main/java/ind/josefswadijohansson/weatherapp/WeatherAppCli.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ind.josefswadijohansson.weatherapp;
+
+/**
+ *
+ * @author Josef
+ */
+public class WeatherAppCli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
