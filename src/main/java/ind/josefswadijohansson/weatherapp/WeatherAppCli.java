4bbcdc2d0108ce45fb723e62c1d22fef67e1865d
josefswadijohansson <swadijosef@gmail.com>
2025-02-10 13:30:56 +0100

    added an executable, and made the searching looped

--- src/main/java/ind/josefswadijohansson/weatherapp/WeatherAppCli.java
+++ src/main/java/ind/josefswadijohansson/weatherapp/WeatherAppCli.java
@@ -10,25 +10,31 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class WeatherAppCli {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("WEATHER_APIKEY");
+        
         try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("");
+            System.out.println("==============================================");
+            System.out.println("To cancel out of the program, press CTRL+C");
+            System.out.println("==============================================");
 
-            Dotenv dotenv = Dotenv.load();
+            while(true){
+                System.out.println(""); // To space out the question from the top
+                System.out.print("What city do you want the weather forecast for? ");
+        
+                String cityName = scanner.nextLine().trim();
 
-            System.out.println(""); // To space out the question from the top
-            System.out.print("What city do you want the weather forecast for? ");
-    
-            String cityName = scanner.nextLine().trim();
-    
-            String apiKey = dotenv.get("WEATHER_APIKEY");
-    
-            Coordinates cityCoordinates = WeatherService.getCoordinatesFromCityName(cityName, apiKey);
-    
-            System.out.println("\n============ Current Weather ===========");
-            WeatherService.printCurrentWeather(cityName, apiKey);
-            System.out.println("============ Weather Forecast ===========");
-            WeatherService.printWeatherForecast(cityCoordinates);
-            System.out.println("=========================================");
-    
+                if(cityName.isBlank() || cityName.isEmpty()) continue;
+        
+                Coordinates cityCoordinates = WeatherService.getCoordinatesFromCityName(cityName, apiKey);
+        
+                System.out.println("\n============ Current Weather ===========");
+                WeatherService.printCurrentWeather(cityName, apiKey);
+                System.out.println("============ Weather Forecast ===========");
+                WeatherService.printWeatherForecast(cityCoordinates);
+                System.out.println("=========================================");
+            }
         } catch (Exception error) {
             System.out.println("Error: " + error.getMessage());
         }
