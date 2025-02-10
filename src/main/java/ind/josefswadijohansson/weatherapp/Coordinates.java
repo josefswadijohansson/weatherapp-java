0b92433cff4b42834eb0f1d43125361477216f26
josefswadijohansson <swadijosef@gmail.com>
2025-02-10 10:56:00 +0100

    implemented so you get 7-day forecast outputted

---
+++ src/main/java/ind/josefswadijohansson/weatherapp/Coordinates.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ind.josefswadijohansson.weatherapp;
+
+/**
+ *
+ * @author Josef
+ */
+public class Coordinates {
+    public final String latitude;
+    public final String longtitude;
+
+    public Coordinates(String latitude, String longitude){
+        this.latitude = latitude;
+        this.longtitude = longitude;
+    }
+}
